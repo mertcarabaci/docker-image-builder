c62e8efffcf2c0e90657ae49f2e3e23121a1171d
Mert Arabacı <73842886+mertcarabaci@users.noreply.github.com>
2023-12-24 05:23:20 +0300

    Update tag.bash

--- .github/workflows/tag.bash
+++ .github/workflows/tag.bash
@@ -1,9 +1,8 @@
 #!/usr/bin/tag.bash
 
 LAST_ID=$(git tag -l |  sort -V | tail -n 1)
-echo ${LAST_ID}
 if [[ -n "${LAST_ID}" ]] ; then
-    LAST_IDX=$(echo "${LAST_ID}" | sed -E "'s/^v\([0-9]\+\)\.\([0-9]\+\)-\([0-9]\+\)/\3/'")
+    LAST_IDX=$(echo "${LAST_ID}" | sed 's/^v\([0-9]\+\)\.\([0-9]\+\)-\([0-9]\+\)/\3/')
     NEXT_ID=$(( LAST_IDX + 1 ))
 else
     NEXT_ID=0
