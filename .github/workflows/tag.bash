5648410897d50441d05a1bf4d1cf8e0c865e430c
Mert Arabaci <arabaci19@itu.edu.tr>
2023-12-24 04:03:37 +0300

    test

---
+++ .github/workflows/tag.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/tag.bash
+
+LAST_ID=$(git tag -l |  sort -V | tail -n 1)
+echo "LastID: $LAST_ID"
+if [[ -n "${LAST_ID}" ]] ; then
+    LAST_IDX=$(echo "${LAST_ID}" | sed -E "'s/^v\([0-9]\+\)\.\([0-9]\+\)-\([0-9]\+\)/\3/'")
+    NEXT_ID=$(( LAST_IDX + 1 ))
+else
+    NEXT_ID=0
+fi
+
+echo $NEXT_ID
